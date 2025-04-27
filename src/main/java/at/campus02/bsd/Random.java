3dd46e59ce6df92720eb08860118551d0259fc72
Hannah Posch <s54209@edu.campus02.at>
2025-04-27 17:51:51 +0200

    ex4_1 picture created

---
+++ src/main/java/at/campus02/bsd/Random.java
@@ -0,0 +1,10 @@
+package at.campus02.bsd;
+
+public class Random {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Graz");
+        System.out.println(args[0]);
+    }
+}
